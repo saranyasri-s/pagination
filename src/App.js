c807f015d53fe50cfbd2593708ca9e10020da79a
saranyasris <dentist.apassionateprogrammer@gmail.com>
2021-12-24 14:37:02 +0530

    added pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
+import classes from "./App.module.css";
 function App() {
+  const [items, setItems] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const itemsPerPage = 10;
+  const pagesVisitedSoFar = pageNumber * itemsPerPage;
+  const displayItems = items.slice(
+    pagesVisitedSoFar,
+    pagesVisitedSoFar + itemsPerPage
+  );
+  const pageChangeHandler = (data) => {
+    console.log(data.selected);
+    setPageNumber(data.selected);
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await res.json();
+      console.log(data);
+      setItems(data);
+    };
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.items}>
+      {displayItems.map((item) => {
+        return (
+          <div>
+            <h3>{item.id}</h3>
+            <h3>{item.title}</h3>
+          </div>
+        );
+      })}
+
+      <ReactPaginate
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        pageCount={10}
+        onPageChange={pageChangeHandler}
+        
+      />
     </div>
   );
 }
